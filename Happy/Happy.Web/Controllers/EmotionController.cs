84164be8f561176eb0f0e6823cec4de45af9d1ff
vikekh <vikekhse@gmail.com>
2015-03-22 00:46:00 +0100

    Added AddEmotion() method to EmotionController

--- Happy/Happy.Web/Controllers/EmotionController.cs
+++ Happy/Happy.Web/Controllers/EmotionController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace Happy.Web.Controllers
 {
     public class EmotionController : ApiController
     {
+        public async Task<int> AddEmotion(int emotion)
+        {
+            return emotion;
+        }
     }
 }
