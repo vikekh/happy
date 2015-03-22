dbf46e592eed01849047984c61f0dd595c881d9b
vikekh <vikekhse@gmail.com>
2015-03-22 21:33:32 +0100

    Trying to construct a POST

--- Happy/Happy.Web/Controllers/EmotionsController.cs
+++ Happy/Happy.Web/Controllers/EmotionsController.cs
@@ -1,19 +1,20 @@
-﻿using System;
+﻿using Happy.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace Happy.Web.Controllers
 {
     public class EmotionsController : ApiController
     {
-        [HttpPost]
-        public async Task<int> AddEmotion(int emotion)
+        public int PostEmotion(Emotion emotion)
         {
-            return emotion;
+            return emotion.NormalizedValue;
         }
     }
 }
---
+++ Happy/Happy.Web/Models/Emotion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Happy.Web.Models
+{
+    public class Emotion
+    {
+        public int NormalizedValue {
+            get {
+                return Value - 3;
+            }
+        }
+
+        private int value;
+        public int Value {
+            get {
+                if (value < 1)
+                {
+                    return 1;
+                }
+                else if (value > 5)
+                {
+                    return 5;
+                }
+
+                return value;
+            }
+
+            set
+            {
+                this.value = value;
+            }
+        }
+    }
+}
